a10c54634727eb996a35683f9f9d23306054c107
AndresQuijano <andresquijanoarroyave@gmail.com>
2020-11-06 15:19:09 -0500

    Created first page (index). Uploaded CSS and font. Create first controller for index.

--- src/main/java/com/cheessemess/CheessemessApplication.java
+++ src/main/java/com/cheessemess/CheessemessApplication.java
@@ -1,4 +1,4 @@
-package com.cheessemess.cheessemess;
+package com.cheessemess;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
