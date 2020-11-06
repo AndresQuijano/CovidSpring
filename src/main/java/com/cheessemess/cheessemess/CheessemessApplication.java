4d418025e2526ec7601fcd8b3dc970acd6d08832
AndresQuijano <andresquijanoarroyave@gmail.com>
2020-11-06 14:24:32 -0500

    Initial commit. Created project using Initializr

---
+++ src/main/java/com/cheessemess/cheessemess/CheessemessApplication.java
@@ -0,0 +1,13 @@
+package com.cheessemess.cheessemess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheessemessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheessemessApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cheessemess/cheessemess/CheessemessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cheessemess.cheessemess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheessemessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
